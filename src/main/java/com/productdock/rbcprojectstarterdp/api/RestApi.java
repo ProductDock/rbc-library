9c618dc031c05ae2ec15ff47ba913d2e5b91912e
Dusan <dusan.petrovic@productdock.com>
2022-03-02 14:14:42 +0100

    Modified rest end points

--- src/main/java/com/productdock/rbcprojectstarterdp/api/RestApi.java
+++ src/main/java/com/productdock/rbcprojectstarterdp/api/RestApi.java
@@ -1,6 +1,7 @@
 package com.productdock.rbcprojectstarterdp.api;
 
 import com.productdock.rbcprojectstarterdp.Member;
+import com.productdock.rbcprojectstarterdp.model.Member;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,19 +13,23 @@ import java.util.List;
 @RequestMapping("/members")
 public class RestApi {
 
-    private List<Member> allMembers = new ArrayList<Member>();
+    public static List<Member> members = new ArrayList<>() {
+        {
+            add(new Member("Milica", "Milica"));
+        }
+    };
 
     @GetMapping
     public List<Member> getAll(){
-        return allMembers;
+        return members;
     }
 
     @PostMapping
     public Member addMember(@RequestBody Member member){
         if(member != null){
-            allMembers.add(member);
+            members.add(member);
             return member;
         }
         return null;
-    }
+    };
 }
--- src/test/java/com/productdock/rbcprojectstarterdp/RestApiTest.java
+++ src/test/java/com/productdock/rbcprojectstarterdp/RestApiTest.java
@@ -1,10 +1,12 @@
 package com.productdock.rbcprojectstarterdp;
 
 import com.productdock.rbcprojectstarterdp.api.RestApi;
+import com.productdock.rbcprojectstarterdp.model.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,13 +17,14 @@ public class RestApiTest {
     private RestApi restApi;
 
     @Test
-    public void addMember() {
-        Member member = new Member("Nenad", "Becanovic");
-        assertThat(restApi.addMember(member)).isEqualTo(member);
+    public void get() {
+        assertThat(restApi.getAll().size()).isEqualTo(1);
     }
 
     @Test
-    public void getAllMembers() {
-        assertThat(restApi.getAll().size()).isEqualTo(0);
+    public void post() {
+        int size = restApi.members.size();
+        restApi.addMember(new Member("proba", "proba"));
+        assertThat(restApi.members.size()).isEqualTo(++size);
     }
 }
