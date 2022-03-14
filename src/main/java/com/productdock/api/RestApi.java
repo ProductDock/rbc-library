277bf667363fb67ad38e241ef79b3d7ef7c8d63e
vukomanv-pd <vladimir.vukoman@productdock.com>
2022-03-14 11:07:24 +0100

    Generate QR codes feature added

--- src/main/java/com/productdock/api/RestApi.java
+++ src/main/java/com/productdock/api/RestApi.java
@@ -1,7 +1,6 @@
 package com.productdock.api;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.productdock.util.MethodUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test")
 public class RestApi {
+
+    private final String imagePath = "./src/main/resources/qrcodes/QRCode.png";
+
+    @GetMapping("/generateImageQRCode")
+    public String getQRCode() {
+        MethodUtils.generateImageQRCode("https://productdock.com/", 250, 250, imagePath);
+        return "QR code created";
+    }
 
     @GetMapping
     public String get() {
