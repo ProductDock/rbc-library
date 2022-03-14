0e5b48c59a04bab60742a2882740d21fbfff64d9
Dusan <dusan.petrovic@productdock.com>
2022-03-14 10:12:08 +0100

    Added member dto

--- src/main/java/com/productdock/api/RestApi.java
+++ src/main/java/com/productdock/api/RestApi.java
@@ -12,6 +12,18 @@ import java.util.UUID;
 @RequestMapping("/members")
 public class RestApi {
 
+    static class MemberDTO {
+        public String firstName;
+        public String lastName;
+
+        public MemberDTO() {}
+
+        public MemberDTO(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+
     static class Member {
         public UUID id;
         public String firstName;
@@ -19,16 +31,16 @@ public class RestApi {
 
         public Member() {}
 
-        public Member(UUID id, String firstName, String lastName) {
-            this.id = id;
+        public Member(String firstName, String lastName) {
+            this.id = UUID.randomUUID();
             this.firstName = firstName;
             this.lastName = lastName;
         }
     }
     List<Member> members = new ArrayList<Member>() {
         {
-            add(new Member(UUID.randomUUID(),"Pera", "Peric"));
-            add(new Member(UUID.randomUUID(),"Nikola", "Nikolic"));
+            add(new Member("Pera", "Peric"));
+            add(new Member("Nikola", "Nikolic"));
         }
     };
 
@@ -40,9 +52,9 @@ public class RestApi {
 
     @PostMapping
     @CrossOrigin
-    public ResponseEntity<?> create(@RequestBody Member member) {
-        member.id = UUID.randomUUID();
-        members.add(member);
-        return new ResponseEntity<>(member.id, HttpStatus.CREATED);
+    public ResponseEntity<?> create(@RequestBody MemberDTO memberDTO) {
+        Member mem = new Member(memberDTO.firstName, memberDTO.lastName);
+        members.add(mem);
+        return new ResponseEntity<>(mem.id, HttpStatus.CREATED);
     }
 }
