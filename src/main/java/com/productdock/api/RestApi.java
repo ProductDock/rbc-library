1407d8fb879744c9a5e5175d96a2867e8274d92e
NenadBecanovicPD <nenad@nenads-MacBook-Pro.local>
2022-03-21 15:16:13 +0100

    Test sonar quality gate

--- src/main/java/com/productdock/api/RestApi.java
+++ src/main/java/com/productdock/api/RestApi.java
@@ -5,6 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping("/api/test")
