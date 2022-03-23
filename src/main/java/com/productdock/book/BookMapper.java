38f53eaad8484fce0f3a04c3908c9e41822f4751
Milica Zivkov <milica.zivkov@productdock.com>
2022-03-23 13:11:04 +0100

    optimize imports

--- src/main/java/com/productdock/book/BookMapper.java
+++ src/main/java/com/productdock/book/BookMapper.java
@@ -3,8 +3,6 @@ package com.productdock.book;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
-import java.awt.print.Book;
-
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface BookMapper {
 
--- src/main/java/com/productdock/book/BookService.java
+++ src/main/java/com/productdock/book/BookService.java
@@ -1,6 +1,5 @@
 package com.productdock.book;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
