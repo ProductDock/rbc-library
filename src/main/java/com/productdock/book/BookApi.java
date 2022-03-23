7004ee57a96aac94a87ec0ac79176f0b12be41e3
Milica Zivkov <milica.zivkov@productdock.com>
2022-03-23 13:07:44 +0100

    remove unused imports

--- src/main/java/com/productdock/book/BookApi.java
+++ src/main/java/com/productdock/book/BookApi.java
@@ -1,6 +1,5 @@
 package com.productdock.book;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
