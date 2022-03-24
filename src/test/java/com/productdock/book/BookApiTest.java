3cc050db46978d9ede57b05c5eae32893bf78ac0
Milica Zivkov <milica.zivkov@productdock.com>
2022-03-24 13:50:56 +0100

    tests should pass authentication with mocked user

--- src/test/java/com/productdock/book/BookApiTest.java
+++ src/test/java/com/productdock/book/BookApiTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,7 @@ class BookApiTest {
   }
 
   @Test
+  @WithMockUser
   void getAll_whenBooksExists() throws Exception {
     givenThatBooksAreInDatabase();
 
@@ -45,6 +47,7 @@ class BookApiTest {
   }
 
   @Test
+  @WithMockUser
   void getAll_whenNoBooks() throws Exception {
     mockMvc.perform(get("/api/books"))
             .andExpect(status().isOk())
