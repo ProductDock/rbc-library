1fefb55544bbe88cfe7a5f26d23564186d4d4762
Milica Zivkov <milica.zivkov@productdock.com>
2022-03-25 11:43:44 +0100

    convert BookDto to a Data Structure

--- src/main/java/com/productdock/book/BookDto.java
+++ src/main/java/com/productdock/book/BookDto.java
@@ -1,17 +1,12 @@
 package com.productdock.book;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.io.Serializable;
 
-@Getter
-@Setter
 public class BookDto implements Serializable {
 
-    private Long id;
-    private String title;
-    private String author;
-    private String cover;
+    public Long id;
+    public String title;
+    public String author;
+    public String cover;
 
 }
