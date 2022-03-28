1371ce40991c69e12f25088bc8c04ee148cfa9c0
NenadBecanovicPD <100684660+NenadBecanovicPD@users.noreply.github.com>
2022-03-28 09:16:50 +0200

    Add option to configure allowed origin through properties

--- src/main/java/com/productdock/config/WebConfig.java
+++ src/main/java/com/productdock/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.productdock.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,9 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed.origins}")
+    private String corsAllowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins(corsAllowedOrigins);
     }
 }
