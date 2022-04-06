81a4871135067497c0e127324bc1e4485142e795
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-04-06 15:29:28 +0200

    Test full deploy

--- update-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker stop rbc-library
-docker rm rbc-library
-docker rmi gcr.io/prod-pd-library/prod-pd-library-app-image:rbc-library
-docker run -dp 8080:8080 --rm --name=rbc-library gcr.io/prod-pd-library/prod-pd-library-app-image:rbc-library
