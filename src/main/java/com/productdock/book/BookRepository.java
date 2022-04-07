45c004834d9d0a90fe70424ae22066280c0b7b7f
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-07 16:02:40 +0200

    Resolved 'code smell' in SQL text section

--- src/main/java/com/productdock/book/BookRepository.java
+++ src/main/java/com/productdock/book/BookRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 public interface BookRepository extends PagingAndSortingRepository<BookEntity, Long> {
 
     @Query(value = """
-            select b.* from book b\r\n
-            left join book_topic bt on b.id = bt.book_id\r\n
-            left join topic t on t.id = bt.topic_id\r\n
+            select b.* from book b 
+            left join book_topic bt on b.id = bt.book_id 
+            left join topic t on t.id = bt.topic_id 
             where t.name in (?1)
             """,
             nativeQuery = true)
