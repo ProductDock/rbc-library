ac8869f6e141b311b087f2a66e9d720d4865f9c2
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-07 15:56:52 +0200

    Resolved 'code smell' in SQL text in repository

--- src/main/java/com/productdock/book/BookRepository.java
+++ src/main/java/com/productdock/book/BookRepository.java
@@ -9,10 +9,14 @@ import java.util.List;
 
 public interface BookRepository extends PagingAndSortingRepository<BookEntity, Long> {
 
-    @Query( value= "select b.* from book b\r\n " +
-                    "left join book_topic bt on b.id = bt.book_id\r\n " +
-                    "left join topic t on t.id = bt.topic_id\r\n" +
-                    "where t.name in (?1)",
-    nativeQuery = true)
+    @Query(value = """
+            select b.* from book b\r\n
+            left join book_topic bt on b.id = bt.book_id\r\n
+            left join topic t on t.id = bt.topic_id\r\n
+            where t.name in (?1)
+            """,
+            nativeQuery = true)
     Page<BookEntity> findAllByTopicsName(List<String> filters, Pageable pageable);
 }
+
+
