ffa1cc875bb183e08acbe041a1f8dd60618e62bf
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-07 15:15:45 +0200

    added a method for filtering books by topics in the repository

--- src/main/java/com/productdock/book/BookRepository.java
+++ src/main/java/com/productdock/book/BookRepository.java
@@ -1,6 +1,18 @@
 package com.productdock.book;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface BookRepository extends PagingAndSortingRepository<BookEntity, Long> {
+
+    @Query( value= "select b.* from book b\r\n " +
+                    "left join book_topic bt on b.id = bt.book_id\r\n " +
+                    "left join topic t on t.id = bt.topic_id\r\n" +
+                    "where t.name in (?1)",
+    nativeQuery = true)
+    Page<BookEntity> findAllByTopics_Name(List<String> filters, Pageable pageable);
 }
