9f247346e0e467e99b0003ada13bafbc3e713865
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-07 15:25:05 +0200

    return all books or filtered based on input param

--- src/main/java/com/productdock/book/BookService.java
+++ src/main/java/com/productdock/book/BookService.java
@@ -2,6 +2,7 @@ package com.productdock.book;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -11,10 +12,26 @@ public record BookService(BookRepository bookRepository,
 
     private static final int PAGE_SIZE = 18;
 
-    public List<BookDto> getBooks(int page) {
+    public List<BookDto> getBooks(List<String> topics, int page) {
+        if(CollectionUtils.isEmpty(topics)){
+            return getAllBooks(page);
+        }
+        return getBooksByTopics(topics, page);
+    }
+
+    private List<BookDto> getAllBooks(int page){
         var pageTemplate = PageRequest.of(page, PAGE_SIZE);
         return bookRepository
                 .findAll(pageTemplate)
+                .stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+
+    private List<BookDto> getBooksByTopics(List<String> topics, int page){
+        var pageTemplate = PageRequest.of(page, PAGE_SIZE);
+        return bookRepository
+                .findAllByTopics_Name(topics, pageTemplate)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();
