040139935d9ad2904141d96ff445eadb8d36f402
Milica Zivkov <milica.zivkov@productdock.com>
2022-04-12 22:22:55 +0200

    spring profile is not a secret

--- prod-env/update-catalog-image.sh
+++ prod-env/update-catalog-image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo usermod -a -G docker ${USER}
 docker-credential-gcr configure-docker
+
 docker stop rbc-library
 docker rm rbc-library
-docker rmi gcr.io/prod-pd-library/prod-pd-library-app-image:rbc-library
-docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/pd-library/credentials.json -dp 8080:8080 --name=rbc-library gcr.io/prod-pd-library/prod-pd-library-app-image:rbc-library
-docker container ls -a
+docker rmi gcr.io/prod-pd-library/rbc-library:latest
+
+export GOOGLE_APPLICATION_CREDENTIALS=/home/pd-library/credentials.json
+docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/credentials.json -dp 8080:8080 --name=rbc-library gcr.io/prod-pd-library/rbc-library:latest
