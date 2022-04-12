908f83fb867dce82566ec81cac832f44f6f85e7b
Milica Zivkov <milica.zivkov@productdock.com>
2022-04-12 11:08:35 +0200

    single query handles both filtering by topics and returning non filtered results

--- src/main/java/com/productdock/book/BookService.java
+++ src/main/java/com/productdock/book/BookService.java
@@ -4,8 +4,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static org.springframework.util.CollectionUtils.isEmpty;
+import java.util.Optional;
 
 @Service
 public record BookService(BookRepository bookRepository,
@@ -13,26 +12,10 @@ public record BookService(BookRepository bookRepository,
 
     private static final int PAGE_SIZE = 18;
 
-    public List<BookDto> getBooks(List<String> topics, int page) {
-        if (isEmpty(topics)) {
-            return getAllBooks(page);
-        }
-        return getBooksByTopics(topics, page);
-    }
-
-    private List<BookDto> getAllBooks(int page) {
+    public List<BookDto> getBooks(Optional<List<String>> topics, int page) {
         var pageTemplate = PageRequest.of(page, PAGE_SIZE);
         return bookRepository
-                .findAll(pageTemplate)
-                .stream()
-                .map(bookMapper::toDto)
-                .toList();
-    }
-
-    private List<BookDto> getBooksByTopics(List<String> topics, int page) {
-        var pageTemplate = PageRequest.of(page, PAGE_SIZE);
-        return bookRepository
-                .findAllByTopicsName(topics, pageTemplate)
+                .findByTopicsName(topics, pageTemplate)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();
--- src/test/java/com/productdock/book/BookApiTest.java
+++ src/test/java/com/productdock/book/BookApiTest.java
@@ -1,6 +1,7 @@
 package com.productdock.book;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,9 +10,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import static com.productdock.book.data.provider.BookEntityMother.defaultBook;
+import static com.productdock.book.data.provider.BookEntityMother.defaultBookBuilder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,7 +21,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles({"in-memory-db"})
 class BookApiTest {
 
-    public static final int NUMBER_OF_BOOKS = 19;
+    public static final int RESULTS_PAGE_SIZE = 19;
+    public static final String FIRST_PAGE = "0";
+    public static final String SECOND_PAGE = "1";
 
     @Autowired
     private MockMvc mockMvc;
@@ -35,106 +38,95 @@ class BookApiTest {
 
     @Test
     @WithMockUser
-    void getSecondPage_whenBooksExists() throws Exception {
-        givenFirstPageOfResults();
-        givenSecondPageOfResults();
-
-        mockMvc.perform(get("/api/books").param("page", "1"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("[{\"id\": " + NUMBER_OF_BOOKS + ",\"title\":\"Second Page Title\",\"author\":\"Second Page Author\",\"cover\":null}]"));
-    }
-
-    private void givenSecondPageOfResults() {
-        var book = new BookEntity();
-        book.setAuthor("Second Page Author");
-        book.setTitle("Second Page Title");
-        bookRepository.save(book);
-    }
-
-    private void givenFirstPageOfResults() {
-        for (int i = 0; i < NUMBER_OF_BOOKS - 1; i++) {
-            givenThatBookIsInDatabase();
-        }
-    }
-
-    private void givenThatBookIsInDatabase() {
-        var book = new BookEntity();
-        book.setAuthor("Clean Architecture");
-        book.setTitle("Robert C. Martin");
-        bookRepository.save(book);
-    }
-
-    @Test
-    @WithMockUser
-    void getFirstPageByTopics_whenNoBooksExists() throws Exception {
-        mockMvc.perform(get("/api/books")
-                        .param("page", "0")
-                        .param("topics","MARKETING")
-                        .param("topics","DESIGN"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("[]"));
-    }
-
-    @Test
-    @WithMockUser
-    void getSecondPageByTopics_whenNoBooksExists() throws Exception {
-        mockMvc.perform(get("/api/books")
-                        .param("page", "1")
-                        .param("topics","MARKETING")
-                        .param("topics","DESIGN"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("[]"));
-    }
-
-    @Test
-    @WithMockUser
-    void getFirstPageByTopics_whenBooksExists() throws Exception {
-        givenThatFilteredBooksAreInDb();
-
-        mockMvc.perform(get("/api/books")
-                        .param("page", "0")
-                        .param("topics","MARKETING")
-                        .param("topics","DESIGN"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].title").value("Title Marketing"))
-                .andExpect(jsonPath("$[1].title").value("Title Design"));
-    }
-
-    private void givenThatFilteredBooksAreInDb() {
-        givenABookInTopic("PRODUCT", "Title Product");
-        givenABookInTopic("MARKETING", "Title Marketing");
-        givenABookInTopic("DESIGN", "Title Design");
-    }
-
-    private void givenABookInTopic(String topicName, String title) {
-        Set<TopicEntity> topics = new HashSet<>();
-        var topic = new TopicEntity();
-        topic.setName(topicName);
-
-        var book = new BookEntity();
-        book.setTitle(title);
-        book.setAuthor("Author");
-        topics.add(topic);
-        book.setTopics(topics);
-
-        bookRepository.save(book);
-    }
-
-    @Test
-    @WithMockUser
-    void getFirstPage_whenNoBooksExists() throws Exception {
-        mockMvc.perform(get("/api/books").param("page", "0"))
-                .andExpect(status().isOk())
-                .andExpect(content().json("[]"));
-    }
-
-    @Test
-    @WithMockUser
-    void countAllBooks_whenBooksExist() throws Exception {
-        givenThatBookIsInDatabase();
+    void countAllBooks() throws Exception {
+        givenAnyBook();
 
         mockMvc.perform(get("/api/books/count"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("1"));
     }
+
+    private void givenAnyBook() {
+        var book = defaultBook();
+        bookRepository.save(book);
+    }
+
+    @Nested
+    class SearchWithFilters {
+
+        @Test
+        @WithMockUser
+        void getSecondPage_whenEmptyResults() throws Exception {
+            mockMvc.perform(get("/api/books")
+                            .param("page", SECOND_PAGE)
+                            .param("topics", "MARKETING")
+                            .param("topics", "DESIGN"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("[]"));
+        }
+
+        @Test
+        @WithMockUser
+        void getFirstPage_whenThereAreResults() throws Exception {
+            givenABookBelongingToTopic("PRODUCT", "Title Product");
+            givenABookBelongingToTopic("MARKETING", "Title Marketing");
+            givenABookBelongingToTopic("DESIGN", "Title Design");
+
+            mockMvc.perform(get("/api/books")
+                            .param("page", FIRST_PAGE)
+                            .param("topics", "MARKETING")
+                            .param("topics", "DESIGN"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.*").value(hasSize(2)))
+                    .andExpect(jsonPath("$[0].title").value("Title Marketing"))
+                    .andExpect(jsonPath("$[1].title").value("Title Design"));
+        }
+
+        private void givenABookBelongingToTopic(String topicName, String title) {
+            var topic = new TopicEntity();
+            topic.setName(topicName);
+            var book = defaultBookBuilder().title(title).topic(topic).build();
+
+            bookRepository.save(book);
+        }
+
+    }
+
+    @Nested
+    class GetBooksWithPagination {
+
+        @Test
+        @WithMockUser
+        void getFirstPage_whenEmptyResults() throws Exception {
+            mockMvc.perform(get("/api/books").param("page", FIRST_PAGE))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("[]"));
+        }
+
+        @Test
+        @WithMockUser
+        void getSecondPage_whenThereAreResults() throws Exception {
+            givenFirstPageOfResults();
+            givenSecondPageOfResults();
+
+            mockMvc.perform(get("/api/books").param("page", SECOND_PAGE))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.*").value(hasSize(1)))
+                    .andExpect(jsonPath("$[0].id").exists())
+                    .andExpect(jsonPath("$[0].title").value("Second Page Title"));
+        }
+
+        private void givenFirstPageOfResults() {
+            for (int i = 0; i < RESULTS_PAGE_SIZE - 1; i++) {
+                givenAnyBook();
+            }
+        }
+
+        private void givenSecondPageOfResults() {
+            var book = defaultBookBuilder().title("Second Page Title").build();
+            bookRepository.save(book);
+        }
+
+    }
+
 }
