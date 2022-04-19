291f405506b84349b1429be728184901aba39667
natasatutuspd <100684664+natasatutuspd@users.noreply.github.com>
2022-04-19 15:49:00 +0200

    Rename rbc-library into rbc-library-catalog

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17-jdk-alpine AS builder
-WORKDIR /app/pd-library
+WORKDIR /app
 COPY mvnw .
 COPY .mvn .mvn
 COPY pom.xml .
@@ -12,7 +12,7 @@ ENV SPRING_PROFILES_ACTIVE=$ACTIVE_PROFILE
 ENV SPRING_DATASOURCE_PASSWORD=$PASSWORD
 WORKDIR /app
 COPY entrypoint.sh /entrypoint.sh
-COPY --from=builder /app/pd-library/target/rbc-library-0.0.1-SNAPSHOT.jar rbc-library-0.0.1-SNAPSHOT.jar
+COPY --from=builder /app/target/rbc-library-catalog-0.0.1-SNAPSHOT.jar rbc-library-catalog-0.0.1-SNAPSHOT.jar
 EXPOSE 8080
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- prod-env/update-catalog-image.sh
+++ prod-env/update-catalog-image.sh
@@ -2,9 +2,10 @@
 sudo usermod -a -G docker ${USER}
 docker-credential-gcr configure-docker
 
-docker stop rbc-library
-docker rm rbc-library
-docker rmi gcr.io/prod-pd-library/rbc-library:latest
+docker stop rbc-library-catalog
+docker rm rbc-library-catalog
+docker rmi $(docker images | grep "rbc-library-catalog")
 
 export GOOGLE_APPLICATION_CREDENTIALS=/home/pd-library/credentials.json
-docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/credentials.json -dp 8080:8080 --name=rbc-library gcr.io/prod-pd-library/rbc-library:latest
+docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/credentials.json -dp 8080:8080 --name=rbc-library-catalog gcr.io/prod-pd-library/rbc-library-catalog:$1
+docker container ls -a
