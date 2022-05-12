b466b311d472ba20cdb92dcc810126f6718a2aed
Natasa Tutus <natasa.tutus@productdock.com>
2022-05-12 11:52:04 +0200

    the Recomendation has extended wit bit value

--- src/main/java/com/productdock/book/Recommendation.java
+++ src/main/java/com/productdock/book/Recommendation.java
@@ -2,6 +2,18 @@ package com.productdock.book;
 
 public enum Recommendation {
 
-    JUNIOR, MEDIOR, SENIOR
+    JUNIOR(0),
+    MEDIOR(1),
+    SENIOR(2);
+
+    private int bit;
+
+    Recommendation(int bit) {
+        this.bit = (int) Math.pow(2, bit);
+    }
+
+    public int getBit() {
+        return this.bit;
+    }
 
 }
