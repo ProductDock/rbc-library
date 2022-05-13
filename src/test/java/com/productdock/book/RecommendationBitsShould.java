6bb428c4d3be399a1e8a16db40160118015e4272
Dusan <dusan.petrovic@productdock.com>
2022-05-13 11:59:36 +0200

    Refactor recommendation bits tests to be parametrized

--- src/test/java/com/productdock/book/RecommendationBitsShould.java
+++ src/test/java/com/productdock/book/RecommendationBitsShould.java
@@ -1,34 +1,53 @@
 package com.productdock.book;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.productdock.book.Recommendation.*;
-import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RecommendationBitsShould {
 
-    @Test
-    void convertRecommendationListToInt() {
+    @ParameterizedTest
+    @MethodSource("recommendationListParametersProvider")
+    void convertRecommendationListToInt(List<Recommendation> recommendations, int output) {
+        assertThat(getIntRepresentation(recommendations)).isEqualTo(output);
+    }
 
-        var recommendations = singletonList(SENIOR);
-        assertThat(getIntRepresentation(recommendations)).isEqualTo(4);
-
-        recommendations = List.of(SENIOR, JUNIOR, MEDIOR);
-        assertThat(getIntRepresentation(recommendations)).isEqualTo(7);
+    static Stream<Arguments> recommendationListParametersProvider(){
+        return Stream.of(
+                Arguments.of(List.of(JUNIOR), 1),
+                Arguments.of(List.of(MEDIOR), 2),
+                Arguments.of(List.of(SENIOR), 4),
+                Arguments.of(List.of(JUNIOR, MEDIOR), 3),
+                Arguments.of(List.of(SENIOR, MEDIOR), 6),
+                Arguments.of(List.of(SENIOR, JUNIOR, MEDIOR), 7)
+                );
     }
 
     private int getIntRepresentation(List<Recommendation> recommendationList) {
         return RecommendationBits.from(recommendationList).toInt();
     }
 
-    @Test
-    void convertIntToRecommendationList() {
+    @ParameterizedTest
+    @MethodSource("recommendationBitsParametersProvider")
+    void convertIntToRecommendationList(int intRepresentation, List<Recommendation> output) {
+        assertThat(getRecommendations(intRepresentation)).containsExactlyInAnyOrderElementsOf(output);
+    }
 
-        assertThat(getRecommendations(7)).containsExactlyInAnyOrder(JUNIOR, MEDIOR, SENIOR);
-        assertThat(getRecommendations(1)).containsExactly(JUNIOR);
+    static Stream<Arguments> recommendationBitsParametersProvider(){
+        return Stream.of(
+                Arguments.of(1, List.of(JUNIOR)),
+                Arguments.of(2, List.of(MEDIOR)),
+                Arguments.of(4, List.of(SENIOR)),
+                Arguments.of(3, List.of(JUNIOR, MEDIOR)),
+                Arguments.of(6, List.of(SENIOR, MEDIOR)),
+                Arguments.of(7, List.of(SENIOR, JUNIOR, MEDIOR))
+        );
     }
 
     private List<Recommendation> getRecommendations(int intRepresentation) {
