7de546b91fac3342ad4bf628b41b759972ebf48b
Natasa Tutus <natasa.tutus@productdock.com>
2022-05-13 11:03:38 +0200

    renamed BadRequestException to BookReviewException

--- src/main/java/com/productdock/book/ReviewService.java
+++ src/main/java/com/productdock/book/ReviewService.java
@@ -1,6 +1,6 @@
 package com.productdock.book;
 
-import com.productdock.exception.BadRequestException;
+import com.productdock.exception.BookReviewException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +11,7 @@ public record ReviewService(ReviewRepository reviewRepository,
         var reviewEntity = reviewMapper.toEntity(reviewDto);
 
         if (reviewRepository.existsById(reviewEntity.getReviewCompositeKey())) {
-            throw new BadRequestException("The user cannot enter more than one comment for a particular book.");
+            throw new BookReviewException("The user cannot enter more than one comment for a particular book.");
         }
         reviewRepository.save(reviewEntity);
     }
