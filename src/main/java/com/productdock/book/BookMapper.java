d6e3d9d8b1c0ebadf8d06deba084236371864d62
Dusan <dusan.petrovic@productdock.com>
2022-05-17 08:46:16 +0200

    Remove unnecessary mapping annotation

--- src/main/java/com/productdock/book/BookMapper.java
+++ src/main/java/com/productdock/book/BookMapper.java
@@ -1,14 +1,11 @@
 package com.productdock.book;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring", uses = {ReviewDtoMapper.class})
 public interface BookMapper {
 
-    @Mapping(target = "reviews", source = "source.reviews")
     BookDto toDto(BookEntity source);
-
 }
