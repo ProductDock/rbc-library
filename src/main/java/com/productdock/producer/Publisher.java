85212b0ae084dc043c80e7f6d712d4d0aea1dc4b
Natasa Tutus <natasa.tutus@productdock.com>
2022-05-18 09:50:38 +0200

    added kafka publisher and new topic

---
+++ src/main/java/com/productdock/producer/Publisher.java
@@ -0,0 +1,29 @@
+package com.productdock.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.productdock.book.BookRatingMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class Publisher {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final RecordProducer recordProducer;
+
+    @Value("${spring.kafka.topic.book-rating}")
+    private String kafkaTopic;
+
+    public Publisher(KafkaTemplate<String, String> kafkaTemplate, RecordProducer recordProducer) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.recordProducer = recordProducer;
+    }
+
+    public void sendMessage(BookRatingMessage bookRatingMessage) throws ExecutionException, InterruptedException, JsonProcessingException {
+        var kafkaRecord = recordProducer.createKafkaRecord(kafkaTopic, bookRatingMessage);
+        kafkaTemplate.send(kafkaRecord).get();
+    }
+}
