7967aa609427a596cf93e73c6c481dd533c79fae
Dusan <dusan.petrovic@productdock.com>
2022-05-19 15:30:46 +0200

    Refactored BookMapper topic mapping

--- src/main/java/com/productdock/book/BookMapper.java
+++ src/main/java/com/productdock/book/BookMapper.java
@@ -2,12 +2,21 @@ package com.productdock.book;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+import java.util.Set;
 
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring", uses = {ReviewDtoMapper.class})
 public interface BookMapper {
 
-    @Mapping(target = "topics", expression = "java(source.getTopics().stream().map(TopicEntity::getName).toList())")
+    @Mapping(source = "topics", target = "topics", qualifiedByName = "topicEntitiesToTopicNameList")
     BookDto toDto(BookEntity source);
+
+    @Named("topicEntitiesToTopicNameList")
+    static List<String> topicEntitiesToTopicNameList(Set<TopicEntity> topics) {
+        return topics.stream().map(TopicEntity::getName).toList();
+    }
 }
