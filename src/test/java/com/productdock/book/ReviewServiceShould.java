6a10eef892f23b6d6077fe3b84d138010507f910
Natasa Tutus <natasa.tutus@productdock.com>
2022-05-20 14:26:01 +0200

    refactored

--- src/test/java/com/productdock/book/ReviewServiceShould.java
+++ src/test/java/com/productdock/book/ReviewServiceShould.java
@@ -33,7 +33,7 @@ class ReviewServiceShould {
     private BookRatingCalculator calculator;
 
     @Mock
-    private JsonRecordPublisher ratingJsonRecordPublisher;
+    private JsonRecordPublisher jsonRecordPublisher;
 
     @Mock
     private ReviewMapper reviewMapper;
@@ -62,7 +62,7 @@ class ReviewServiceShould {
     }
 
     private void assertThatValidMessagesPublishedToKafka() throws ExecutionException, InterruptedException, JsonProcessingException {
-        verify(ratingJsonRecordPublisher).sendMessage(any(), bookRatingMessageCaptor.capture());
+        verify(jsonRecordPublisher).sendMessage(any(), bookRatingMessageCaptor.capture());
 
         var bookRatingMessageValue = bookRatingMessageCaptor.getValue();
         assertThat(bookRatingMessageValue.getBookId()).isEqualTo(reviewDtoMock.bookId);
@@ -79,7 +79,7 @@ class ReviewServiceShould {
         reviewService.saveReview(reviewDtoMock);
 
         verify(reviewRepository).save(reviewEntityMock);
-        verify(ratingJsonRecordPublisher, times(0)).sendMessage(eq("book-rating"), any());
+        verify(jsonRecordPublisher, times(0)).sendMessage(eq("book-rating"), any());
     }
 
     @Test
