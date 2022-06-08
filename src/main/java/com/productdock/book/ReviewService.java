af7ee25c2a965105b3368696c0a6c4d156362640
Dusan <dusan.petrovic@productdock.com>
2022-06-08 15:50:48 +0200

    Existing review can be edited

--- src/main/java/com/productdock/book/ReviewService.java
+++ src/main/java/com/productdock/book/ReviewService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -25,12 +27,32 @@ public class ReviewService {
         var reviewEntity = reviewMapper.toEntity(reviewDto);
 
         if (reviewRepository.existsById(reviewEntity.getReviewCompositeKey())) {
-            log.error("The User is trying to add second review.");
+            log.warn("The User with id:{} is trying to add second review for book with id:{}",
+                    reviewDto.userId, reviewDto.bookId);
             throw new BookReviewException("The user cannot enter more than one comment for a particular book.");
         }
         reviewRepository.save(reviewEntity);
         log.debug("Saved a review: [{}]", reviewEntity);
         if (reviewEntity.getRating() == null) {
+            return;
+        }
+        publishNewBookRating(reviewDto.bookId);
+    }
+
+    public void editReview(ReviewDto reviewDto) {
+        var reviewEntity = reviewMapper.toEntity(reviewDto);
+        var existingReview = reviewRepository.findById(reviewEntity.getReviewCompositeKey());
+
+        if (existingReview.isEmpty()) {
+            log.warn("The User with id:{} is trying to edit not existing review with id:[{},{}]",
+                    reviewDto.userId, reviewDto.bookId, reviewDto.userId);
+            throw new BookReviewException("Review not found");
+        }
+        var existingRating = existingReview.get().getRating();
+
+        reviewRepository.save(reviewEntity);
+        log.debug("Edited a review: [{}]", reviewEntity);
+        if (reviewEntity.getRating().equals(existingRating)) {
             return;
         }
         publishNewBookRating(reviewDto.bookId);
