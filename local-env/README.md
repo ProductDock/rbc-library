29e9acb2e66a8562af18af37b6ac0bb2f4f2b060
dusanpetrovicpd <100684501+dusanpetrovicpd@users.noreply.github.com>
2022-08-22 13:33:31 +0200

    Authenticate with our custom jwt token

--- local-env/README.md
+++
@@ -1,3 +0,0 @@
-## Connect to docker's MySQL from host:
-
-`mysql --host=0.0.0.0 --port 3308 --user=root --password=root`
--- prod-env/update-catalog-image.sh
+++ prod-env/update-catalog-image.sh
@@ -7,5 +7,5 @@ docker rm rbc-library-catalog
 docker rmi $(docker images | grep "rbc-library-catalog")
 
 export GOOGLE_APPLICATION_CREDENTIALS=/home/pd-library/credentials.json
-docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/credentials.json -e KAFKA_SERVER_URL=$KAFKA_SERVER_URL -dp 8082:8080 --name=rbc-library-catalog gcr.io/prod-pd-library/rbc-library-catalog:$1
+docker run -v $GOOGLE_APPLICATION_CREDENTIALS:/home/credentials.json:ro -v $CATALOG_JWT_PUBLIC_KEY:/app/app.pub:ro -e GOOGLE_APPLICATION_CREDENTIALS=/home/credentials.json -e KAFKA_SERVER_URL=$KAFKA_SERVER_URL -dp 8082:8080 --name=rbc-library-catalog gcr.io/prod-pd-library/rbc-library-catalog:$1
 docker container ls -a
