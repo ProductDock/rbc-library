43e1378476a5ef708af74f676d08980f0124ebb3
Dusan <dusan.petrovic@productdock.com>
2022-08-25 09:25:11 +0200

    Fixed case when existing review is missing rating and new review have rating

--- src/test/java/com/productdock/application/service/EditBookReviewServiceShould.java
+++ src/test/java/com/productdock/application/service/EditBookReviewServiceShould.java
@@ -4,11 +4,15 @@ import com.productdock.application.port.out.persistence.ReviewPersistenceOutPort
 import com.productdock.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -39,16 +43,25 @@ class EditBookReviewServiceShould {
         verify(reviewRepository).save(review);
     }
 
-    @Test
-    void editReviewWhenRatingEdited() {
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    void editReviewWhenRatingEdited(Short existingRating, Short newRating) {
         var reviewCompositeKey = Book.Review.ReviewCompositeKey.builder().bookId(BOOK_ID).build();
-        var review = Book.Review.builder().rating((short) 5).reviewCompositeKey(reviewCompositeKey).build();
-        var existingReview = Book.Review.builder().rating((short) 3).build();
+        var review = Book.Review.builder().rating(newRating).reviewCompositeKey(reviewCompositeKey).build();
+        var existingReview = Book.Review.builder().rating(existingRating).build();
         given(reviewRepository.findById(review.getReviewCompositeKey())).willReturn(Optional.of(existingReview));
 
         service.editReview(review);
 
         verify(reviewRepository).save(review);
         verify(newRatingPublisher).publishRating(BOOK_ID);
+    }
+
+    static Stream<Arguments> testArguments() {
+        return Stream.of(
+                Arguments.of((short) 5, (short) 3),
+                Arguments.of(null, (short) 3),
+                Arguments.of((short) 5, null)
+        );
     }
 }
