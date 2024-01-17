a908b0e7cc7879a08eaf6780f747009cfc39422f
Milica Zivkov <milica.zivkov@productdock.com>
2024-01-17 13:45:44 +0100

    fix flyway

---
+++ src/main/resources/db/migration/V1_3_1__align_book_73_with_production.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (id, title, author, cover) VALUES (73, 'Deleted in prod', 'Deleted in prod', 'deleted in prod');
+
+DELETE FROM book WHERE id = 73;
--- src/main/resources/local.db.migration.fix/V1_8_1__fix_referencing_non_existant_book.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM book_topic
-WHERE book_id = 86 AND topic_id = 1;
