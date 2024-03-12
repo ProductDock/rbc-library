7f6a1e11e3ff4cdd6fbae65ae462c4b10b122982
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-03-12 14:53:27 +0100

    Fix formatting

--- src/main/java/com/productdock/domain/exception/DeleteBookException.java
+++ src/main/java/com/productdock/domain/exception/DeleteBookException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class DeleteBookException  extends RuntimeException {
+public class DeleteBookException extends RuntimeException {
 
     public DeleteBookException(String message){ super(message);}
 }
